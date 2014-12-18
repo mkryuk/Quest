f471a0b684b2e775674b8361ab5f84a168e254e7
mkryuk <mkryuk@gmail.com>
2014-12-18 14:38:28 +0200

    First question

---
+++ Quest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Quest
+{
+    class Program
+    {
+        public static BigInteger Fact(BigInteger val)
+        {
+            if (val == 0)
+            {
+                return 1;
+            }
+            return val * Fact(--val);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fact(104) * 10 / 3600);
+        }
+    }
+}
