666e48e309f9df403b5f63f8bec52f3377d62e28
mkryuk <mkryuk@gmail.com>
2014-12-18 14:49:56 +0200

    Second question

--- Quest/Program.cs
+++ Quest/Program.cs
@@ -1,25 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
-using System.Numerics;
+using System.Text.RegularExpressions;
 
 namespace Quest
 {
     class Program
     {
-        public static BigInteger Fact(BigInteger val)
-        {
-            if (val == 0)
-            {
-                return 1;
-            }
-            return val * Fact(--val);
-        }
+       
         static void Main(string[] args)
-        {
-            Console.WriteLine(Fact(104) * 10 / 3600);
+        {            
+            const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            var fileStream = new StreamReader("..\\..\\lyrics.result");
+            var text = fileStream.ReadToEnd();
+            var filtered = Regex.Matches(text, "(Yo|Nice)");
+            var result = new StringBuilder();
+            var index = 0;
+            var nice = false;
+            for (var i = 0; i < filtered.Count; i++)
+            {
+                switch (filtered[i].Value)
+                {
+                    case "Yo":
+                        index++;
+                        nice = false;
+                        break;
+                    case "Nice":
+                        //was the previous "Nice"
+                        if (nice)
+                        {
+                            //the end of word
+                            result.Append(" ");
+                            nice = false;
+                        }
+                        else
+                        {
+                            //append the letter to the word
+                            result.Append(alphabet[index - 1]);
+                            index = 0;
+                            nice = true;
+                        }
+                        break;
+                }
+            }
+
+            Console.WriteLine(result);
         }
     }
 }
