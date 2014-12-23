68d81707df9759d21cb0a65f3684a6f93ca12927
mkryuk <mkryuk@gmail.com>
2014-12-23 13:22:33 +0200

    Eighth question

--- Quest/Program.cs
+++ Quest/Program.cs
@@ -1,37 +1,61 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading;
 
 namespace Quest
 {
     class Program
     {
+
         static void Main(string[] args)
-        {
-            var fileStream = new StreamReader("..\\..\\lyrics.data");
-            const string myPart = "xyzw";
-            const string starPart = "abcd";
-            //read all song to end (it`s in one string file)
-            var wholeSong = fileStream.ReadLine();
-            //cut all noise like back vocal ets.
-            var songWithoutBack = Regex.Replace(wholeSong, "[^abcdxyzw]", "");
-            //filter all my parties
-            var myParts = Regex.Matches(songWithoutBack, "[^abcd]+");
-            //filter all star parties
-            var starParts = Regex.Matches(songWithoutBack, "[^xyzw]+");
-            //evaluate my longest party
-            var myMaxLength = myParts.Cast<Match>().Select(match=>match.Value).ToList().Max((item)=>item.Length);
-            //evaluate star longest party
-            var starMaxLength = starParts.Cast<Match>().Select(match => match.Value).ToList().Max((item) => item.Length);
-            //calculate in persentage
-            var myResult = myMaxLength * 100 / (myMaxLength + starMaxLength);
-            var starResult = starMaxLength * 100 / (myMaxLength + starMaxLength);
-            Console.WriteLine("my party longest length: {0}% star party longest length: {1}%", myResult, starResult);
+        {            
+            var fileStream = new StreamReader("..\\..\\musetalks.data");
+            var langList = new List<List<string>>();
+            while (!fileStream.EndOfStream)
+            {
+                var readLine = fileStream.ReadLine();
+                if (readLine == null) continue;
+
+                var line = readLine.Split(' ');
+
+                //looking up in all list of languages
+                var found = langList.FindAll((item) =>
+                {       
+                    var result = false;
+
+                    //looking up read muse in existing list
+                    line.ToList().ForEach((muse) =>
+                    {
+                        var index = item.FindIndex((oneOfMuses) => oneOfMuses == muse);
+                        if (index == -1) return;
+                        //add both muses to their language partners
+                        item.AddRange(line);
+                        result = true;
+                    });
+                    return result;
+                });
+
+                //add muses to the new language list
+                if (found.Count == 0)
+                {
+                    langList.Add(line.ToList());
+                }
+                //in the case both muses exists in different lines of language list 
+                //combine this lines together and remove remaining
+                else if (found.Count > 1)
+                {
+                    found[0].AddRange(found[1]);
+                    langList.Remove(found[1]);
+                }
+            }
+            //find out the longest language chain
+            //distinct is for eliminate muse repetition
+            var longestLanguageChain = langList.Max((list)=>list.Distinct().Count());
+
+            Console.WriteLine("maximum muses chain speaks one language {0}",longestLanguageChain);
         }
     }
 }
