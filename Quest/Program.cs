8253bef6b46d105e4808eef8af8ba7a82acc5fc2
mkryuk <mkryuk@gmail.com>
2015-02-04 15:45:36 +0200

    Nineghteen question (Bad singers in alphabet order)

--- Quest/Program.cs
+++ Quest/Program.cs
@@ -2,79 +2,40 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 
 namespace Quest
 {
-    internal class Circle
-    {
-        public Point CenterPoint { get; set; }
-        public int Radius { get; set; }
-
-    }
+    
 
     class Program
     {
 
         static void Main(string[] args)
         {
-            var fileReader = new StreamReader("..//..//data.dat");
-            var points = new List<Circle>();
-            var singerPoint = new Point();
+            var fileReader = new StreamReader("..//..//names.dat");
+            var names = new Dictionary<string,int>();
             //read file and load data
             while (!fileReader.EndOfStream)
             {
-                var coords = fileReader.ReadLine().Split(' ');
-                switch (coords.Length)
-                {
-                    case 3:
-                        points.Add(new Circle()
-                        {                            
-                                CenterPoint = new Point(Int32.Parse(coords[0]), Int32.Parse(coords[1])),
-                                Radius = Int32.Parse(coords[2])}
-                        );
-                        break;
-                    case 2:
-                        singerPoint.X = Int32.Parse(coords[0]);
-                        singerPoint.Y = Int32.Parse(coords[1]);
-                        break;
-                }
+                var name = fileReader.ReadLine();
+                if (names.ContainsKey(name))
+                    names[name]++;
+                
+                else                
+                    names.Add(name,1);
             }
             fileReader.Close();
-
+            //select all bad singers in alphabet order
+            var result = from name in names where name.Value == names.Max(item=>item.Value) orderby name.Key select name.Key;
 
             using (var writer = new StreamWriter("..//..//result.txt"))
             {
-                var position = 1;
-                var score = 0;
-                var result = new StringBuilder();
-                points.ForEach(circle =>
-                {
-                    //calculate radius from center point of the circle to singer by Pythagoras' theorem
-                    var tempRadius = Math.Pow(circle.Radius, 2);
-                    var radToSinger = Math.Pow(Math.Abs(circle.CenterPoint.X - singerPoint.X), 2) +
-                                      Math.Pow(Math.Abs(circle.CenterPoint.Y - singerPoint.Y), 2);
-                    //if singer in the circle or on it's border 
-                    //add position to result and calculate the score
-                    if (radToSinger <= tempRadius)
-                    {
-                        score += 499;
-                        result.Append(String.Format("{0} ", position));
-                    }
-                    position++;
-                });
-                if (result.Length == 0)
-                {
-                    //if singer didn't fail
-                    result.Append("0");
-                }
-                else
-                {
-                    result.Append(score);
-                }
-                //trim spaces and write result
-                writer.Write(result.ToString());
+                var strResult = new StringBuilder();
+                result.ToList().ForEach(item => strResult.Append(String.Format("{0}, ", item)));
+                writer.WriteLine(strResult.ToString().Trim(new []{' ',','}));
             }
         }
     }
